e44b3badd2c6764208eb9cc5347c9d279eb2bfdd
juanjosegb <juanjose.garciabeza@hotmail.com>
2020-06-12 14:44:33 +0200

    [ADD] Migrations

---
+++ reactamorty-api/Migrations/20200612124157_InitialCreate.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace reactamorty_api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "episode",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    air_date = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    episode = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    url = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    created = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_episode", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "location",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    air_date = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    episode = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    url = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    created = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_location", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    status = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    species = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    type = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    gender = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    image = table.Column<string>(type: "varchar(45)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                        .Annotation("MySql:Collation", "utf8mb4_0900_ai_ci"),
+                    created = table.Column<DateTime>(type: "datetime", nullable: true),
+                    origin = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_origin",
+                        column: x => x.origin,
+                        principalTable: "location",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character_has_episode",
+                columns: table => new
+                {
+                    character_id = table.Column<int>(nullable: false),
+                    episode_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.character_id, x.episode_id });
+                    table.ForeignKey(
+                        name: "fk_character_has_episode_character1",
+                        column: x => x.character_id,
+                        principalTable: "character",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_character_has_episode_episode1",
+                        column: x => x.episode_id,
+                        principalTable: "episode",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "location_has_character",
+                columns: table => new
+                {
+                    location_id = table.Column<int>(nullable: false),
+                    character_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.location_id, x.character_id });
+                    table.ForeignKey(
+                        name: "fk_location_has_character_character1",
+                        column: x => x.character_id,
+                        principalTable: "character",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_location_has_character_location1",
+                        column: x => x.location_id,
+                        principalTable: "location",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "fk_origin_idx",
+                table: "character",
+                column: "origin");
+
+            migrationBuilder.CreateIndex(
+                name: "fk_character_has_episode_character1_idx",
+                table: "character_has_episode",
+                column: "character_id");
+
+            migrationBuilder.CreateIndex(
+                name: "fk_character_has_episode_episode1_idx",
+                table: "character_has_episode",
+                column: "episode_id");
+
+            migrationBuilder.CreateIndex(
+                name: "fk_location_has_character_character1_idx",
+                table: "location_has_character",
+                column: "character_id");
+
+            migrationBuilder.CreateIndex(
+                name: "fk_location_has_character_location1_idx",
+                table: "location_has_character",
+                column: "location_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "character_has_episode");
+
+            migrationBuilder.DropTable(
+                name: "location_has_character");
+
+            migrationBuilder.DropTable(
+                name: "episode");
+
+            migrationBuilder.DropTable(
+                name: "character");
+
+            migrationBuilder.DropTable(
+                name: "location");
+        }
+    }
+}
