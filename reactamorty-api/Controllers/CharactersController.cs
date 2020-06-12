bd49838eb6f18698d1cb420aa6b4e8eb74f2d5c4
juanjosegb <juanjose.garciabeza@hotmail.com>
2020-06-12 14:47:10 +0200

    [FIX] Format code

--- reactamorty-api/Controllers/CharactersController.cs
+++ reactamorty-api/Controllers/CharactersController.cs
@@ -27,9 +27,7 @@ namespace reactamorty_api.Controllers
         public async Task<ActionResult<CharactersDto>> Get(int page = 1, string name = "", string status = "",
             string species = "", string type = "", string gender = "")
         {
-            var characterData =
-                new CharacterData(page == 0 ? 1 : page, name ?? "", status ?? "", species ?? "", type ?? "",
-                    gender ?? "");
+            var characterData = new CharacterData(page == 0 ? 1 : page, name ?? "", status ?? "", species ?? "", type ?? "", gender ?? "");
             var characters = await _characterService.GetCharacters(characterData);
 
             if (page > Math.Ceiling((double) characters.Count / 20))
@@ -38,19 +36,15 @@ namespace reactamorty_api.Controllers
             }
 
             var paginatedCharacters = characters.GetRange(20 * (characterData.Page - 1),
-                Math.Abs(characters.Count - (20 * (characterData.Page - 1))) >= 20
-                    ? 20
-                    : Math.Abs(characters.Count - (20 * (characterData.Page - 1))));
+                Math.Abs(characters.Count - (20 * (characterData.Page - 1))) >= 20 ? 20 : Math.Abs(characters.Count - (20 * (characterData.Page - 1))));
 
             var charactersDto = _mapper.Map<List<Character>, CharactersDto>(paginatedCharacters, opt =>
             {
                 opt.AfterMap((src, dest) =>
                 {
                     dest.Info.Pages = Math.Ceiling((double) characters.Count / 20);
-                    dest.Info.Next =
-                        characterData.Page + 1 > dest.Info.Pages ? null : _characterService.FormatCharacterUrl(characterData, -1);
-                    dest.Info.Prev =
-                        characterData.Page - 1 < 1 ? null : _characterService.FormatCharacterUrl(characterData, 1);
+                    dest.Info.Next = characterData.Page + 1 > dest.Info.Pages ? null : _characterService.FormatCharacterUrl(characterData, -1);
+                    dest.Info.Prev = characterData.Page - 1 < 1 ? null : _characterService.FormatCharacterUrl(characterData, 1);
                 });
             });
             return charactersDto;
